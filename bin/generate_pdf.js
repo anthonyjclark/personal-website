1c324bde71d1cddb6cd61895c87973c1b21d3a1b
Anthony J. Clark <ajc@>
2019-10-18 08:12:13 -0500

    Updated print CV css.

--- bin/generate_pdf.js
+++ bin/generate_pdf.js
@@ -1,12 +1,17 @@
+const build_mode = process.env.BUILD_MODE;
+
 const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // https://people.missouristate.edu/AnthonyClark/cv/
-    await page.goto('https://people.missouristate.edu/AnthonyClark/cv/', {
-        waitUntil: 'networkidle2'
-    });
+    const url = (build_mode === "release")
+        ? 'https://people.missouristate.edu/AnthonyClark/cv/'
+        : 'http://localhost:8080/cv/';
+
+    console.log(url);
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
     await page.pdf({ path: 'cv.pdf' });
 
     await browser.close();
